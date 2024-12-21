2f292bf07cf6213101350aa3945a78a5857e5707
Ashen1919 <2303S0834@saegis.ac.lk>
2024-12-21 23:07:49 +0530

    created hero section of the home page

---
+++ Customer/Home Page/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sandaru Food Mart</title>
+
+    <!-- Favicons -->
+    <link
+        href="https://cloud.appwrite.io/v1/storage/buckets/6766627e001d33e3cf21/files/6766629c000934fe54e2/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
+        rel="icon">
+    <link
+        href="https://cloud.appwrite.io/v1/storage/buckets/6766627e001d33e3cf21/files/6766629c000934fe54e2/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
+        rel="apple-touch-icon">
+
+    <!-- Css Stylesheets -->
+    <link href="../Assets/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <!--Header-->
+    <header>
+        <!--Top Bar-->
+        <div class="topBar">
+            <div class="contact-details">
+                <ul class="contact">
+                    <a href="#">
+                        <li><i class="bi bi-envelope-fill text-primary"></i>sandarufoodmart@gmail.com</li>
+                    </a>
+                    <li><i class="bi bi-telephone-fill text-primary"></i>+94 33 267 8970</li>
+                </ul>
+            </div>
+            <div class="searchBar">
+                <form action="" method="get" class="search-form">
+                    <input type="search" name="search" id="search" placeholder="Search Here">
+                    <button type="submit" class="search-button">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="social-icon">
+                <a href="#"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="whatsapp"><i class="bi bi-whatsapp"></i></a>
+                <a href="#"><i class="bi bi-linkedin"></i></a>
+            </div>
+        </div>
+        <!--End of Top Bar-->
+
+        <!--Nav Bar-->
+        <div class="navbar">
+            <div class="logo">
+                <a href=""><img
+                        src="https://cloud.appwrite.io/v1/storage/buckets/6766627e001d33e3cf21/files/6766629c000934fe54e2/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
+                        alt="logo">
+                    <p>Sandaru Food <span class="mart">Mart</span></p>
+                </a>
+            </div>
+            <nav id="navmenu" class="navmenu">
+                <ul>
+                    <li><a href="#hero" class="active">Home<br></a></li>
+                    <li><a href="#about">About us</a></li>
+                    <li class="dropdown"><a href="#"><span>Categories</span> <i
+                                class="bi bi-chevron-down toggle-dropdown"></i></a>
+                        <ul>
+                            <li><a href="#">Grocery</a></li>
+                            <li><a href="#">Vegetables</a></li>
+                            <li><a href="#">Fruits</a></li>
+                            <li><a href="#">Beverages</a></li>
+                            <li><a href="#">Household</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#contact">Contact us</a></li>
+                </ul>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
+            <div class="right-side-icons">
+                <a class="profile" href=""><i class="bi bi-person-circle "></i></a>
+                <a class="wishlist" href=""><i class="bi bi-heart  "></i></i></a>
+                <a class="cart" href=""><i class="bi bi-cart4 "></i></a>
+            </div>
+        </div>
+        <!--End of Nav Bar-->
+    </header>
+    <!--End of header-->
+
+    <!--Hero section-->
+    <div class="hero">
+        <div class="left-card">
+            <div class="left-content">
+                <p class="sub-title">100% Fresh</p>
+                <p class="main-title">Everything you need, All in one place.</p>
+                <p class="description">Sandaru Food Mart offers fresh, high-quality groceries, ensuring excellent
+                    produce, meats, and pantry essentials.</p>
+                <button class="shop-btn">Shop Now <i class="bi bi-arrow-right"></i></button>
+            </div>
+            <div class="right-img">
+                <img src="https://cloud.appwrite.io/v1/storage/buckets/6766627e001d33e3cf21/files/6766c9da002d58a19516/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
+                    alt="Card-image">
+            </div>
+        </div>
+        <div class="right-card">
+            <div class="right-top-card">
+                <div class="left-text">
+                    <p class="sub-text">20% OFF</p>
+                    <p class="main-text">Fruits & Vegetables</p>
+                    <p>Sandaru Food Mart delivers fresh, high-quality fruits and vegetables.</p>
+                    <button class="shop-btn">Shop Now <i class="bi bi-arrow-right"></i></button>
+                </div>
+                <div class="right-side-img">
+                    <img src="https://cloud.appwrite.io/v1/storage/buckets/6766627e001d33e3cf21/files/6766e05b00173cb93d66/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
+                        alt="Card-image">
+                </div>
+            </div>
+            <div class="right-bottom-card">
+                <div class="left-bottom-text">
+                    <p class="sub-bottom-text">15% OFF</p>
+                    <p class="main-bottom-text">Shop fresh, shop smart</p>
+                    <p>Top-quality, reliable, durable house essentials.</p>
+                    <button class="shop-btn">Shop Now <i class="bi bi-arrow-right"></i></button>
+                </div>
+                <div class="right-side-bottom-img">
+                    <img src="https://cloud.appwrite.io/v1/storage/buckets/6766627e001d33e3cf21/files/6766ea840013ba06342b/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin"
+                        alt="Card-image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--End of the hero section-->
+
+    <script src="../Assets/js/script.js"></script>
+</body>
+
+</html>
