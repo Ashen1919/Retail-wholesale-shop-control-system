de68fe46ae05697b24df6221096b297161fad5f0
Ashen1919 <2303S0834@saegis.ac.lk>
2024-12-23 23:04:43 +0530

    begin to create admin dashboard

--- Customer/Home Page/index.php
+++ Customer/Home Page/index.php
@@ -18,7 +18,6 @@
     <link href="../Assets/css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Sinhala:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
