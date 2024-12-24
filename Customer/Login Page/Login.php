f514cecb6f1750d302a1ced6f0410587759d64b4
S.S.M.Chathuranganee <sandunichathurangi18@gmail.com>
2024-12-24 16:57:55 +0530

    update login page

---
+++ Customer/Login Page/Login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sandaru Food Mart</title>
+
+    <!-- Favicons -->
+    <link
+        href="../Assets/images/logo.png"
+        rel="icon">
+    <link
+        href="../Assets/images/logo.png"
+        rel="apple-touch-icon">
+
+    <!-- Css Stylesheets -->
+    <link href="../Assets/css/Loginstyle.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+<div class="wrapper">
+    <from action="">
+    <h1>Log In</h1>
+        <div class="input-box">
+            <i class='bx bxs-envelope'></i>
+            <label for="email">Email</label><br>
+            <input type="email" id="email" placeholder="Enter your email" required>
+            
+        </div>
+        <div class="input-box">
+            <i class='bx bxs-lock-alt'></i>
+            <label for="password">Password</label><br>
+            <input type="password" id="password" placeholder="Enter your password" required>
+            
+            </div>
+        </div>
+        <a href="#" class="forgot-password">Forgot Password?</a><br>
+        <button type="submit" class="login-button">Login</button>
+    </form>
+    <p class="signup-text">
+        Don't have an account? <a href="#">Sign up now</a>
+    </p>
+
+</div>
+
+</body>
+</html>
