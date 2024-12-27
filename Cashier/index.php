f365f217a39d54d7cdb230a5837848f025b75cfb
Disura Sandaruwan <disurasandaruwan0@gmail.com>
2024-12-28 04:51:50 +0530

    Cashier Dashboard - menubar - structure

---
+++ Cashier/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cashier Dashboard</title>
+    <link href="./Assets/css/index.css" rel="stylesheet">
+</head>
+<body>
+    <div class="dashboard">
+        <div class="menu">
+            <div class="menu-header">
+                <h1>Cashier Dashboard</h1>
+            </div>
+
+            <div class="menu-buttons">
+                <button class="counter-button">Counter</button>
+                <button class="statistics-button">Statistics</button>
+            </div>
+
+            <div class="menu-profile">
+                <div class="profile-image">
+                    <img src="Assets/images/person.png" alt="Cashier Profile Picture">
+                </div>
+                <div class="profile-details">
+                    <h3>Cashier Name</h3>
+                    <button class="logout-button">Logout</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+        </div>
+    </div>
+</body>
+</html>
