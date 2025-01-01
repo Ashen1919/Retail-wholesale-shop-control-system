b64da7c895da735d21edf3d4fec9097cbea92f68
Ashen1919 <2303S0834@saegis.ac.lk>
2025-01-01 23:22:34 +0530

    admin dashboard cards created

--- Admin/Dashboard/dashboard.php
+++ Admin/Dashboard/dashboard.php
@@ -1,13 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Dashboard Overview</h2>
-<p>Welcome to the admin dashboard.</p>
+    <title>Sandaru Food Mart</title>
 
+    <!-- Favicons -->
+    <link href="../Assets/images/logo.png" rel="icon">
+    <link href="../Assets/images/logo.png" rel="apple-touch-icon">
+
+    <!-- Css Stylesheets -->
+    <link href="../Assets/css/dashboards.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+</head>
+
+<body>
+    <h2>Dashboard </h2>
+    <div class="content">
+        <div class="left-side-dash">
+            <h4>Today Overview</h4>
+            <div class="dash-cards">
+                <div class="card-1">
+                    <h5>Earnings</h5>
+                </div>
+                <div class="card-2"></div>
+                <div class="card-3"></div>
+            </div>
+        </div>
+        <div class="right-side-dash">
+            <h4>Today Overview</h4>
+            <div class="dash-cards">
+                <div class="card-1">
+                    <h5>Earnings</h5>
+                </div>
+                <div class="card-2"></div>
+                <div class="card-3"></div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
