08211a9a652be44677ebc177fe9b4dcd5ee2f7eb
S.S.M.Chathuranganee <sandunichathurangi18@gmail.com>
2025-01-02 03:52:40 +0530

    update Account Informstion page

--- Customer/Account information/Account I.php
+++ Customer/Account information/Account I.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sandaru Food Mart</title>
+
+    <!-- Favicons -->
+    <link
+        href="../Assets/images/logo.png"
+        rel="icon">
+    <link
+        href="../Assets/images/logo.png"
+        rel="apple-touch-icon">
+
+    <!-- Css Stylesheets -->
+    <link href="../Assets/css/Account.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
 <div class="account-container">
   <h1>Account Information</h1>
   <form>
@@ -27,8 +49,15 @@
     </div>
     <div class="buttons">
       <button type="button" class="edit">Edit</button>
-      <button type="button" class="logout">Log Out</button>
-      <button type="button" class="delete">Delete Account</button>
-    </div>
+</div>
+<div class="Log Out">
+            <a href="logout.php">Log Out</a>
+            </div>
+<div class="detete">
+            <a href="delete_account">Detete Account</a>
+            </div>
   </form>
 </div>
+
+</body>
+</html>
