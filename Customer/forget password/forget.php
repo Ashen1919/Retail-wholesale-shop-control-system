45761c8d284409045286ef9e80f82a5be84b5e40
maleeshanethmini <156651460+maleeshanethmini@users.noreply.github.com>
2025-01-03 09:08:21 +0530

    Create forget.php

---
+++ Customer/forget password/forget.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sandaru Food Mart</title>
+
+    <!-- Favicons -->
+    <link
+        href="../Assets/images/logo.png"
+        rel="icon">
+    <link
+        href="../Assets/images/logo.png"
+        rel="apple-touch-icon">
+
+    <!-- Css Stylesheets -->
+    <link href="../Assets/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+<body>
+    
+</body>
+</html>
