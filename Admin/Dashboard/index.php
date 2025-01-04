f822a3f7221c6267f8897a6aced8085772df9ab8
Ashen1919 <2303S0834@saegis.ac.lk>
2025-01-04 10:27:07 +0530

    create categories section's table

--- Admin/Dashboard/index.php
+++ Admin/Dashboard/index.php
@@ -11,7 +11,7 @@
     <link href="../Assets/images/logo.png" rel="apple-touch-icon">
 
     <!-- Css Stylesheets -->
-    <link href="../Assets/css/styles.css" rel="stylesheet">
+    <link href="../Assets/css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
