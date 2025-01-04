e0563f48665a03a71c61e75070c7a2c2714dbb81
Ashen1919 <2303S0834@saegis.ac.lk>
2025-01-04 12:49:19 +0530

    small change

--- Admin/Dashboard/testing/test.php
+++ Admin/Dashboard/testing/test.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>food mart</title>
+    <title>sandaru food mart</title>
 </head>
 <body>
     
